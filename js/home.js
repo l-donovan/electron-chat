dbd79040096f7ea50a453bd456ac57c2f2474293
l-donovan <ladbuilder1@gmail.com>
2016-02-03 21:44:12 -0500

    chat framework, css

--- js/home.js
+++ js/home.js
@@ -1,15 +1,18 @@
 var fs = require('fs');
 var path = __dirname + "/json";
-var chatsJSON = fs.readFileSync(path + "/chats.json");
-var chats = JSON.parse(chatsJSON);
-var sidebar = $("#leftSidebar");
+var chats = JSON.parse(fs.readFileSync(path + "/chats.json"));
+var sidebar = $("#chatTable");
+
+function loadChat(chatId) {
+  console.log("Opened chat" + chatId);
+}
 
 function createChatElement(id) {
   var result = $.grep(chats['chats'], function(e) { return e.id === id; })[0];
   sidebar.append(`
-    <div class="chat-side-container">
-      <img class="chat-side-image" src="img/chat/${result.image}"></img>
-      <span class="chat-side-title">${result.title}</span>
-    </div>
+    <tr class="chat-side-item">
+      <td style="width: .1px;"><img class="chat-side-image" src="img/chat/${result.image}"></img></td>
+      <td onclick="loadChat(${result.id})"><div class="chat-side-title">${result.title}</div></td>
+    </tr>
   `);
 }
