c1bf7a0358a5ece0628b242242cf706782d1516b
l-donovan <ladbuilder1@gmail.com>
2016-02-08 20:29:59 -0500

    main chat window framework

--- js/home.js
+++ js/home.js
@@ -1,18 +1,30 @@
 var fs = require('fs');
 var path = __dirname + "/json";
-var chats = JSON.parse(fs.readFileSync(path + "/chats.json"));
-var sidebar = $("#chatTable");
 
-function loadChat(chatId) {
-  console.log("Opened chat" + chatId);
+function generateChatSidebar() {
+  var chats = JSON.parse(fs.readFileSync(path + "/chats.json"))['chats'];
+  for (i = 0; i < chats.length; i++) {
+    result = chats[i];
+    $("#chat-side-table").append(`
+      <tr class="chat-side-item">
+        <td style="width: .1px;"><img class="chat-side-image" src="img/chat/${result.image}"></img></td>
+        <td onclick="generateChat(${result.id})"><div class="chat-side-title">${result.title}</div></td>
+      </tr>
+    `);
+  }
 }
 
-function createChatElement(id) {
-  var result = $.grep(chats['chats'], function(e) { return e.id === id; })[0];
-  sidebar.append(`
-    <tr class="chat-side-item">
-      <td style="width: .1px;"><img class="chat-side-image" src="img/chat/${result.image}"></img></td>
-      <td onclick="loadChat(${result.id})"><div class="chat-side-title">${result.title}</div></td>
-    </tr>
-  `);
+function generateChat(id) {
+  var chatMeta = JSON.parse(fs.readFileSync(path + "/chatData/chat_" + id + ".json"))['chatMeta'];
+  var chatHistory = JSON.parse(fs.readFileSync(path + "/chatData/chat_" + id + ".json"))['chatHistory'];
+
+  if($.trim($("#mainPane").html())) $("#mainPane").empty();
+  $("#mainPane").append(`
+    <div id="chat-active-header">
+      <span id="chat-active-header-text" title="Chat ID: ${chatMeta.id}">${chatMeta.title}</span>
+    </div>
+    <div id="chat-active-content">
+      TODO
+    </div>
+  `)
 }
