fbecffa774c77e9e27ecc9389ca4ca27ceb02098
l-donovan <ladbuilder1@gmail.com>
2016-02-10 01:13:02 -0500

    nightly 02/09/16

--- js/home.js
+++ js/home.js
@@ -18,13 +18,21 @@ function generateChat(id) {
   var chatMeta = JSON.parse(fs.readFileSync(path + "/chatData/chat_" + id + ".json"))['chatMeta'];
   var chatHistory = JSON.parse(fs.readFileSync(path + "/chatData/chat_" + id + ".json"))['chatHistory'];
 
-  if($.trim($("#mainPane").html())) $("#mainPane").empty();
-  $("#mainPane").append(`
+  if($.trim($("#pane-main").html())) $("#pane-main").empty();
+  $("#pane-main").append(`
     <div id="chat-active-header">
       <span id="chat-active-header-text" title="Chat ID: ${chatMeta.id}">${chatMeta.title}</span>
     </div>
-    <div id="chat-active-content">
-      TODO
-    </div>
-  `)
+    <div id="chat-active-content"><table id="chat-active-content-table"></table></div>
+  `);
+
+  for (i = 0; i < chatHistory.length; i++) {
+    $("#chat-active-content-table").append(`
+      <tr class="chat-active-content-item">
+        <td><img class="chat-active-content-item-image" src="img/user/${chatHistory[i].author}.jpg"></img></td>
+        <td><div class="chat-active-content-item-content">${chatHistory[i].content.data}</div></td>
+        <td><div class="chat-active-content-item-like">♡</div></td>
+      </tr>
+    `)
+  }
 }
